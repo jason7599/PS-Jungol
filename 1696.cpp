85f08faa8645865b5d805f63a14e4395e519d275
jason7599 <jason7599@hanmail.net>
2026-07-16 14:43:38 +0900

    update

---
+++ 1696.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for (int i = 0, _n = n; i < _n; i++)
+#define RANGE(i, s, e) for (int i = s, _e = e; i <= _e; i++)
+#define REP(n) for (int _ = 0, _n = n; _ < _n; _++)
+#define DBG(x) cerr << #x << " = " << x << '\n';
+#define OUT(x) cout << x;
+#define SP cout << ' ';
+#define NL cout << '\n';
+using namespace std;
+using pii = pair<int, int>;
+template<typename T = int> T input() { T t; cin >> t; return t; }
+template<typename T> T& input(T& t) { cin >> t; return t; }
+template<typename... Args> void input(Args&... args) { ((cin >> args), ...); }
+template<typename... Args> tuple<Args...> inputs() { tuple<Args...> t; apply([](auto&... args){input(args...);}, t); return t; }
+template<typename T, int C> array<T, C> inputs() { array<T, C> arr; for (T& t : arr) cin >> t; return arr; }
+template<typename... Args> void print(const Args&... args) { ((cout << args << ' '), ...); cout << '\n'; }
+template<typename T> T& upmax(T& v, const T& other) { return v = max(v, other); }
+template<typename T> T& upmin(T& v, const T& other) { return v = min(v, other); }
+const pii DIRS[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // drul
+
+int g_sz;
+bool grid[50][50];
+int dists[50][50];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    input(g_sz);
+    FOR(y, g_sz) {
+        FOR(x, g_sz) {
+            grid[y][x] = input<char>() == '1';
+            dists[y][x] = INT_MAX;
+        }
+    }
+
+    deque<pii> dq;
+    dq.push_front({0, 0});
+    dists[0][0] = 0;
+
+    while (dq.size()) {
+        auto [y, x] = dq.front();
+        dq.pop_front();
+
+        if (y == g_sz - 1 && y == x) break;
+
+        for (const auto& [dy, dx] : DIRS) {
+            int ny = y + dy;
+            int nx = x + dx;
+
+            if (min(ny, nx) < 0 || g_sz <= max(ny, nx)) continue;
+
+            int w = !grid[ny][nx];
+            if (dists[ny][nx] > dists[y][x] + w) {
+                dists[ny][nx] = dists[y][x] + w;
+                if (w) {
+                    dq.push_back({ny, nx});
+                } else {
+                    dq.push_front({ny, nx});
+                }
+            }
+        }
+    }
+
+    print(dists[g_sz - 1][g_sz - 1]);
+}
